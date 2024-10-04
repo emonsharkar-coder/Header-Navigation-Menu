c3058541b27f20227e7ccce778eba72d4f29cd8d
emonsharkar-coder <you@example.comsmdemonsharkar@gmail.com>
2024-10-04 21:07:20 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle Menu
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
